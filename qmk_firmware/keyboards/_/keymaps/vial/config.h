f078c1e42bbb6a1f9dd2954f5800100a53a54f92
ha1fis1nd <147615540+ha1fis1nd@users.noreply.github.com>
2025-02-22 12:18:40 +0900

    Delete qmk_firmware/keyboards/_ directory

--- qmk_firmware/keyboards/_/keymaps/vial/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define VIAL_KEYBOARD_UID {0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14}
-#define VIAL_TAP_DANCE_ENTRIES 8
-#define VIAL_COMBO_ENTRIES 8
-#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
